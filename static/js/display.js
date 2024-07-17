d6156c5c490c59c79c694a95f944bbc4986c4941
Carlos Sales <carlossalesti@gmail.com>
2024-07-17 00:00:55 -0300

    Inicia implementação de interface com usuário

--- static/js/display.js
+++ static/js/display.js
@@ -23,3 +23,28 @@ function goToPage(page) {
     window.location.href = page;
 }
 
+function interactionMessage(message) {
+    interactionMsg = message;
+    document.getElementById("divInteractionMsg").innerHTML = interactionMsg;
+}
+
+function interactionImage(status) {
+    switch (status) {
+        case 'thinking':
+            document.getElementById("divInteractionImage").style.display = "none";
+            document.getElementById("divSpinner").style.display = "block";
+            break;
+        case 'talking':
+            document.getElementById("divSpinner").style.display = "none";
+            document.getElementById("interactionImage").src = "/static/images/wavesOn.gif";
+            document.getElementById("divInteractionImage").style.display = "block";            
+            break;            
+
+        default:
+            break;
+    }
+}
+
+function displayMic() {
+    document.getElementById("divMic").style.display = "block";
+}
